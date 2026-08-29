28ec58816b25a71d4ffe64d00422583eeb0f2605
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-29 21:59:17 +0500

    add

--- style.js
+++ style.js
@@ -1,10 +1,21 @@
 const serach = document.querySelector('.serach')
 const inputClose = document.querySelector('.input-close')
 const serachKatta =document.querySelector('.serach-katta')
+// *login
+const login = document.querySelector('.login')
+const loginKatta = document.querySelector('.login-katta')
+const loginClose = document.querySelector('.login.close')
 
 serach.addEventListener("click", () => {
     serachKatta.classList.add("active")
 })
 inputClose.addEventListener("click", () => {
     serachKatta.classList.remove("active")
+})
+// * login
+login.addEventListener("click", () => {
+    loginKatta.classList.add("active")
+})
+loginClose.addEventListener("click", () => {
+    loginKatta.classList.remove("active")
 })
