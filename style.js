3a91bdf920967f47873b76ec6379b60d15c0dba8
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-31 00:07:38 +0500

    add

--- style.js
+++ style.js
@@ -5,6 +5,8 @@ const serachKatta =document.querySelector('.serach-katta')
 const login = document.querySelector('.login')
 const loginKatta = document.querySelector('.login-katta')
 const loginClose = document.querySelector('.login-close')
+// * btn
+const btn = document.getElementById('btn')
 
 serach.addEventListener("click", () => {
     serachKatta.classList.add("active")
@@ -18,4 +20,15 @@ login.addEventListener("click", () => {
 })
 loginClose.addEventListener("click", () => {
     loginKatta.classList.remove("active")
+})
+// * btn
+btn.addEventListener("click", () => {
+    document.body.classList.toggle("dark")
+
+    if (document.body.classList.contains("dark")) {
+        btn.textContent = "🌑"
+    }
+    else {
+        btn.textContent = "☀️"
+    }
 })
